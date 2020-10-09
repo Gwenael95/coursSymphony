fc3b759309431ce97700e145f1ca5413f3e219ba
Gwenael95 <gwenael.marchetti-waternaux@edu.itescia.fr>
2020-10-09 12:21:13 +0200

    separate Gittlabservices

--- src/Controller/GitlabController.php
+++ src/Controller/GitlabController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\Form\Type\TeamSelectUniqueType;
 use App\Services\GitlabServices;
+use App\Services\TeamServices;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -34,18 +35,18 @@ class GitlabController  extends AbstractController
      * redirect to getMergesByTeam/id with team id
      * @Route("/selectTeamMerges",  name="selectTeamMerges")
      * @param Request $request
-     * @param GitlabServices $gitlabServices
+     * @param TeamServices $teamServices
      * @return Response
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function selectTeamMerges(Request $request, GitlabServices $gitlabServices): Response
+    public function selectTeamMerges(Request $request, TeamServices $teamServices): Response
     {
         $form=$this->createForm(TeamSelectUniqueType::class/*, $team*/);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()){
-            $teamId = $gitlabServices->getTeamIdSelectUnique($this->getDoctrine()->getManager(), $request->request->all());
+            $teamId = $teamServices->getTeamIdSelectUnique($this->getDoctrine()->getManager(), $request->request->all());
             return $this->redirectToRoute('getMergesByTeam', ["id"=>$teamId]);
         }
         $content = $this->twig->render("Home/selectTeam.html.twig", array("formTeam"=>$form->createView()));
@@ -56,18 +57,19 @@ class GitlabController  extends AbstractController
      * this function get all merges depending of the selected team (in 'selectTeamMerges' page)
      * @Route("/getMergesByTeam/{id}",  name="getMergesByTeam")
      * @param GitlabServices $gitlabServices
+     * @param TeamServices $teamServices
      * @param int $id
      * @return Response
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function getMergesByTeam( GitlabServices $gitlabServices, int $id): Response
+    public function getMergesByTeam( GitlabServices $gitlabServices, TeamServices $teamServices, int $id): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
         $merges = $gitlabServices->getMergesFromTeam($entityManager, $id);
         $content = $this->twig->render("Home/displayAllMerges.html.twig", array("merges" => $merges,
-            "team"=>$gitlabServices->getTeamById($entityManager, $id)));
+            "team"=>$teamServices->getTeamById($entityManager, $id)));
         return new Response($content);
     }
 
@@ -93,15 +95,15 @@ class GitlabController  extends AbstractController
     /**
      * this function display a team list from database
      * @Route("/getTeam", methods={"GET"} , name="getTeam")
-     * @param GitlabServices $gitlabServices
+     * @param TeamServices $teamServices
      * @return Response
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function getTeam(GitlabServices $gitlabServices): Response
+    public function getTeam(TeamServices $teamServices): Response
     {
-        $teams = $gitlabServices->getAllTeam($this->getDoctrine()->getManager());
+        $teams = $teamServices->getAllTeam($this->getDoctrine()->getManager());
         $content = $this->twig->render("Home/displayTeam.html.twig", ["teams" =>  $teams] );
         return new Response($content);
     }
--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -8,6 +8,7 @@ use App\Form\Type\CreateTeamType;
 use App\Form\Type\TeamProjectAssignType;
 use App\Form\Type\TeamSelectMultipleType;
 use App\Services\GitlabServices;
+use App\Services\TeamServices;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -39,19 +40,19 @@ class TeamController extends AbstractController
      * this will create a new team thanks to a form with a textField input for team name
      * @Route("/createTeam",  name="createTeam")
      * @param Request $request
-     * @param GitlabServices $gitlabServices
+     * @param TeamServices $teamServices
      * @return Response
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function createTeam(Request $request, GitlabServices $gitlabServices): Response
+    public function createTeam(Request $request, TeamServices $teamServices): Response
     {
         $team = new Team();
         $form=$this->createForm(CreateTeamType::class, $team);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()){
-            $gitlabServices->addTeam($this->getDoctrine()->getManager(), $team);
+            $teamServices->addTeam($this->getDoctrine()->getManager(), $team);
         }
 
         $content = $this->twig->render("Home/createTeam.html.twig", array("formTeam"=>$form->createView()));
@@ -64,19 +65,19 @@ class TeamController extends AbstractController
      * to update it
      * @Route("/setTeam",  name="setTeam")
      * @param Request $request
-     * @param GitlabServices $gitlabServices
+     * @param TeamServices $teamServices
      * @return Response
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function setTeam(Request $request, GitlabServices $gitlabServices): Response
+    public function setTeam(Request $request, TeamServices $teamServices): Response
     {
         $team = new Team();
         $form=$this->createForm(TeamSelectMultipleType::class, $team);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()){
-            $teamId = $gitlabServices->getTeamIdSelectMultiple($this->getDoctrine()->getManager(), $request->request->all());
+            $teamId = $teamServices->getTeamIdSelectMultiple($this->getDoctrine()->getManager(), $request->request->all());
             return $this->redirectToRoute('updateTeam', ["id"=>$teamId]);
         }
         $content = $this->twig->render("Home/setTeam.html.twig", array("formTeam"=>$form->createView()));
@@ -87,20 +88,20 @@ class TeamController extends AbstractController
      * this function is used to update a team after having select it (in setTeam)
      * @Route("/updateTeam/{id}",  name="updateTeam")
      * @param Request $request
-     * @param GitlabServices $gitlabServices
+     * @param TeamServices $teamServices
      * @param int $id
      * @return Response
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function updateTeam(Request $request, GitlabServices $gitlabServices, int $id): Response
+    public function updateTeam(Request $request, TeamServices $teamServices, int $id): Response
     {
         $team = new Team();
         $form=$this->createForm(CreateTeamType::class, $team);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()){
-            $gitlabServices->updateTeam($this->getDoctrine()->getManager(), $id, $team);
+            $teamServices->updateTeam($this->getDoctrine()->getManager(), $id, $team);
         }
         $content = $this->twig->render("Home/setTeam.html.twig", array("formTeam"=>$form->createView()));
         return new Response($content);
@@ -112,19 +113,19 @@ class TeamController extends AbstractController
      * know which one we could delete
      * @Route("/delTeam", name="delTeam")
      * @param Request $request
-     * @param GitlabServices $gitlabServices
+     * @param TeamServices $teamServices
      * @return Response
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function delTeam(Request $request, GitlabServices $gitlabServices): Response
+    public function delTeam(Request $request, TeamServices $teamServices): Response
     {
         $team = new Team();
         $form=$this->createForm(TeamSelectMultipleType::class, $team);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()){
-            $gitlabServices->delTeam($this->getDoctrine()->getManager(), $request->request->all());
+            $teamServices->delTeam($this->getDoctrine()->getManager(), $request->request->all());
             return $this->redirectToRoute('delTeam');
         }
         $content = $this->twig->render("Home/delTeam.html.twig", array("formTeam"=>$form->createView()));
@@ -137,12 +138,13 @@ class TeamController extends AbstractController
      * @Route("/assignProject",  name="assignProject")
      * @param Request $request
      * @param GitlabServices $gitlabServices
+     * @param TeamServices $teamServices
      * @return Response
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function assignProject(Request $request, GitlabServices $gitlabServices): Response
+    public function assignProject(Request $request, GitlabServices $gitlabServices, TeamServices $teamServices): Response
     {
         $gitlabServices->updateProject($this->getDoctrine()->getManager());
         $team = new Team();
@@ -150,7 +152,7 @@ class TeamController extends AbstractController
         $form=$this->createForm(TeamProjectAssignType::class, [$team, $project]);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()){
-            $gitlabServices->assignTeamProject($this->getDoctrine()->getManager(),  $request->request->all());
+            $teamServices->assignTeamProject($this->getDoctrine()->getManager(),  $request->request->all());
         }
         $content = $this->twig->render("Home/assignTeamProject.html.twig", array("formTeam"=>$form->createView()));
         return new Response($content);
@@ -167,7 +169,7 @@ class TeamController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function disassignProject(Request $request, GitlabServices $gitlabServices): Response
+    public function disassignProject(Request $request, GitlabServices $gitlabServices, TeamServices $teamServices): Response
     {
         $gitlabServices->updateProject($this->getDoctrine()->getManager());
         $team = new Team();
@@ -175,7 +177,7 @@ class TeamController extends AbstractController
         $form=$this->createForm(TeamProjectAssignType::class, [$team, $project]);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()){
-            $gitlabServices->disassignProject($this->getDoctrine()->getManager(),  $request->request->all());
+            $teamServices->disassignProject($this->getDoctrine()->getManager(),  $request->request->all());
         }
         $content = $this->twig->render("Home/disassignProject.html.twig", array("formTeam"=>$form->createView()));
         return new Response($content);
